123d47148d94c85902e160454323edb59f7ea92d
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2025-09-08 21:18:06 -0700

    Complete Chapter 2: Date Methods and DateTimeFormat API

---
+++ 01-variables-data-types/15-date-object-methods/script.js
@@ -0,0 +1,23 @@
+let x;
+let d = new Date();
+
+x = d.toString();
+
+x = d.getTime();
+x = d.valueOf();
+x = d.getFullYear();
+x = d.getMonth();
+x = d.getDate();
+x = d.getDay();
+x = d.getHours();
+x = d.getMinutes();
+x = d.getSeconds();
+x = d.getMilliseconds;
+x = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
+x = Intl.DateTimeFormat("en-US").format(d);
+x = Intl.DateTimeFormat("en-GB").format(d);
+x = Intl.DateTimeFormat("default").format(d);
+x = Intl.DateTimeFormat("default", { month: "long" }).format(d);
+x = d.toLocaleString("default", { month: "short" });
+
+console.log(x);
