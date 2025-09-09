0443e9fcf3c10e688a7a301be6d1be4e284d85f7
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2025-09-08 17:21:21 -0700

    Complete Chapter 2: Operators

---
+++ 01-variables-data-types/07-operators/script.js
@@ -0,0 +1,46 @@
+// 1. Arithmetic Operators
+let c;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 7 % 5;
+
+// Concatenation
+x = "Hello" + " " + "World";
+
+// Exponent
+x = 2 ** 3;
+
+// Increment
+x = 1;
+x = x + 1;
+x++;
+
+// Decrement
+x = x - 1;
+x--;
+
+// 2. Assigment Operators
+
+x = 10;
+x += 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x %= 5;
+x **= 5;
+
+// 3. Comparison Operators
+x = 2 == "2";
+x = 2 === "2";
+x = 2 != "2";
+x = 2 !== 2;
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
+
+console.log(x, typeof x);
