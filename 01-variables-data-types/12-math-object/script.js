f18c1244822a91a913dde9dd531f2de33c8025b3
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2025-09-08 20:53:08 -0700

    Complete Chapter 2: The Math Object

---
+++ 01-variables-data-types/12-math-object/script.js
@@ -0,0 +1,14 @@
+let x;
+
+x = Math.sqrt(9);
+x = Math.abs(-5);
+x = Math.round(1.7);
+x = Math.ceil(1.2);
+x = Math.floor(1.7);
+x = Math.pow(2, 3);
+x = Math.min(4, 5, 3);
+x = Math.max(4, 5, 3);
+x = Math.random();
+x = Math.floor(Math.random() * 10);
+
+console.log(x);
