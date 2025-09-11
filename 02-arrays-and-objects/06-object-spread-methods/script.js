ecff72d87e5d4db18fab3e0f3e9604bda6f6f849
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2025-09-11 16:21:36 -0700

    Complete Chapter 3: Object Spread Operator and Methods

---
+++ 02-arrays-and-objects/06-object-spread-methods/script.js
@@ -0,0 +1,42 @@
+let x;
+
+const todo = new Object();
+
+todo.id = 1;
+todo.name = "Buy Milk";
+todo.completed = false;
+
+x = todo;
+
+const person = {
+  address: {
+    coords: {
+      lat: 42.234,
+      lng: -72.523,
+    },
+  },
+};
+
+x = person.address.coords.lat;
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { c: 3, d: 4 };
+const obj3 = { ...obj1, ...obj2 };
+const obj4 = Object.assign({}, obj1, obj2);
+const todos = [
+  { id: 1, name: "Buy milk" },
+  { id: 2, name: "Pickup grandma" },
+  { id: 3, name: "Take out trash" },
+];
+
+x = todos[0].name;
+
+x = Object.keys(todo);
+x = Object.keys(todo).length;
+x = Object.values(todo);
+x = Object.entries(todo);
+
+x = todo.hasOwnProperty("name");
+x = todo.hasOwnProperty("age");
+
+console.log(x);
