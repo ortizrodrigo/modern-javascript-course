d197c10768f29ae3d10805bdd019823f2db31a43
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2025-10-11 15:02:18 -0700

    Complete Chapter 9: Remove and Clear Items

---
+++ 08-shopping-list-project/02-remove-clear-items/script.js
@@ -0,0 +1,60 @@
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
+
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
+}
+
+function addItem(e) {
+  e.preventDefault();
+  const newItem = itemInput.value.trim();
+
+  // Validate Input
+  if (newItem === "") {
+    alert("Please add an item");
+    return;
+  }
+
+  // Create list item
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+
+  itemList.appendChild(li);
+
+  itemInput.value = "";
+}
+
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    const icon = e.target;
+    const button = icon.parentElement;
+    const item_li = button.parentElement;
+    item_li.remove();
+  }
+}
+
+function clearItems(e) {
+  while (itemList.firstElementChild) {
+    itemList.removeChild(itemList.firstElementChild);
+  }
+}
+
+// Event Listeners
+itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
