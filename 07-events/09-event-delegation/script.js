de0d5ff668309e7d9d5e66bb35d7c62e8b0004b7
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2025-10-10 21:50:40 -0700

    Complete Chapter 8: Event Delegation and Multiple Events

---
+++ 07-events/09-event-delegation/script.js
@@ -0,0 +1,24 @@
+const listItems = document.querySelectorAll("li");
+const list = document.querySelector("ul");
+
+// listItems.forEach((item) => {
+//   item.addEventListener("click", (e) => {
+//     e.target.remove();
+//   });
+// });
+
+list.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    e.target.remove();
+  }
+});
+list.addEventListener("mouseover", (e) => {
+  if (e.target.tagName === "LI") {
+    e.target.style.color = "red";
+  }
+});
+list.addEventListener("mouseout", (e) => {
+  if (e.target.tagName === "LI") {
+    e.target.style.color = "#333";
+  }
+});
