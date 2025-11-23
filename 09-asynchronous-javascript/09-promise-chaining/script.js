c746d3fd466335efcb5a891803023a4407192cbd
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2025-11-22 23:41:38 -0800

    Complete Chapter 10: Promise Chaining

---
+++ 09-asynchronous-javascript/09-promise-chaining/script.js
@@ -0,0 +1,29 @@
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let error = true;
+
+    if (!error) {
+      resolve({ name: "Joe", age: 22 });
+    } else {
+      reject("Error: Something went wrong :(");
+    }
+  }, 1000);
+});
+
+promise
+  .then((user) => {
+    console.log(user);
+    return user.name;
+  })
+  .then((name) => {
+    console.log(name);
+    return name.length;
+  })
+  .then((nameLength) => {
+    console.log(nameLength);
+  })
+  .catch((error) => {
+    console.log(error);
+    return 123;
+  })
+  .then((x) => console.log("This will run no matter what (since after the catch)", x));
